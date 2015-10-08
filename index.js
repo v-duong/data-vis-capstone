e1b1b801fedd80618a570d304f362144b68fabd2
Vincent Tan <vwtan@umail.ucsb.edu>
2015-10-07 20:07:23 -0700

    attemping database

--- index.js
+++ index.js
@@ -2,11 +2,7 @@ var express = require('express');
 var request = require('request');
 var app = express();
 
-function compile(str, path) {
-  return stylus(str)
-    .set('filename', path)
-    .use(nib())
-}
+app.set('port', (process.env.PORT || 4500));
 
 var userg = "init"
 
@@ -21,9 +17,7 @@ app.get('/', function (req, res) {
   console.log(userg + ' 1');
 });
 
-var server = app.listen(3500, function () {
-  var host = server.address().address;
-  var port = server.address().port;
 
-  console.log('app listening at http://%s:%s', host, port);
+app.listen(app.get('port'), function(){
+  console.log('app now running on port', app.get('port'))
 });
