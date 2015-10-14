5a898a71bcb10fb6e4a3072a3ec8cd22e76e9203
Vincent Tan <vwtan@umail.ucsb.edu>
2015-10-14 00:25:40 -0700

    change DB table

--- index.js
+++ index.js
@@ -4,37 +4,9 @@ var bodyParser = require('body-parser')
 var app = express();
 
 
-//var _ = require('underscore');
-// var mysql =  require('mysql');
-// var connection =  mysql.createConnection({
-//     host : "localhost",
-//     user : "root",
-//     password: "",
-//   });
-
-// // make sure DB is available to connect
-// connection.connect(function(err){
-//   // unable to connect to DB.. Continue W.o DB
-//   if (err){
-//     console.log("DB Error");
-//   }
-//   // able to connect to DB, start query
-//   else {
-//     console.log("DB Good to Go");
-//     connection.query("use healthmessagesexchange4");
-//     var strQuery = "select MsgId, Last_Accessed, patientId, GivenName, FamilyName, BirthTime from messages where patientId > 12530;";
-
-//     connection.query( strQuery, function(err, rows){
-//       if(err) {
-//         throw err;
 
 
-//       }else{
-//         console.log( rows );
-//       }
-//     });
-//   }
-// });
+
 
 var pg = require ('pg');
 //connect to local postgres database
@@ -49,7 +21,7 @@ client.connect(function(err){
   if (err){
     app.locals.dbClient = null;
     console.log("DB ERROR");
-    console.log("Set dbClient to NULL")
+    //console.log("Set dbClient to NULL")
   }
   else {
     app.locals.dbClient = client;
@@ -99,16 +71,7 @@ app.get('/showList',function(req, res){
     console.log("Why!??!");
   else {
 
-  //  var query = client.query("SELECT * FROM firstTable");
-   // query.on('row', function(row) {
-  //    console.log(row);
-  //  });
-   // query.on('end', client.end.bind(client)); //disconnect client manually
-
-
-
-
-    client.query("SELECT * FROM firstTable", function(err, rows){
+    client.query("SELECT * FROM planeinfo", function(err, rows){
       if (err){
         console.log("DB FAILED");
       }
@@ -116,7 +79,7 @@ app.get('/showList',function(req, res){
         var currentRow;
         for (var i in rows.rows){
           currentRow = rows.rows[i];
-          console.log(currentRow);
+          //console.log(currentRow);
         }
         //rows.rows.find({},{},function(e, docs){
           res.render('showList', {
