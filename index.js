bc2c8791bf9b27b003786bdb32065af3d0d4f1bf
Vincent Tan <vwtan@umail.ucsb.edu>
2015-10-18 17:57:53 -0700

    add constraints and list files uploaded

--- index.js
+++ index.js
@@ -45,13 +45,7 @@ app.set('view engine', 'jade');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(bodyParser.urlencoded({
-//   extended: true
-// }));
-// app.use(bodyParser.json());
 
-//app.use(express.json());
-//app.use(express.urlencoded());
 
 app.use(express.static('public'))
 
@@ -94,57 +88,52 @@ app.get('/showList',function(req, res){
 
       }
     });
-
   }
-  //res.end();
-
-
-
-
 });
 
+app.get('/Uploaded_Files', function(req, res){
+  var fileList = fs.readdirSync('public_files');
+ fileList.splice(0,1);
 
+  res.render('Uploaded_Files', {
+    "showFiles" : fileList
+  });
+});
 
 
 app.get('/uploadPage', function(req, res){
 
-  console.log("hi");
   res.render('uploadPage.jade');
   //res.end();
 });
 
 
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public_files/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname );
+  }
+})
 
-var file_uploaded = multer({ dest: 'public_files/' })
-var uploadtype = file_uploaded.single('displayImage');
-app.post('/file-upload', uploadtype, function(req, res, next){
+
+var file_uploaded = multer({ storage: storage });
+
+app.post('/file-upload', file_uploaded.single('displayImage'), function(req, res){
   var tmp_path = req.file.path;
-
-  /** The original name of the uploaded file
-      stored in the variable "originalname". **/
   var target_path = 'public_files/' + req.file.originalname;
-  
-  /** Uploading the file. **/
- 
   var src = fs.createReadStream(tmp_path);
-  var dest = fs.createWriteStream(target_path);
-  var textBuff;
-  src.on('open', function () {
-    // This just pipes the read stream to the response object (which goes to the client)
-    //src.pipe(res);
-    //console.log(textBuff);
-    //console.log(res.file);
-  });
-
   src.on('data', function(fileData){
+    // do the parsing and upload to DB here.. 
+
     textBuff = fileData.toString();
-
   });
-
-  src.pipe(dest);
- 
-  // uploaded successfully
+    // uploaded successfully
   src.on('end', function() { 
+    // remove src??
+
+
     console.log("File Loading Complete!");
     res.render('uploadPage', {
         "fileData" : textBuff
@@ -156,34 +145,7 @@ app.post('/file-upload', uploadtype, function(req, res, next){
   src.on('error', function(err) { res.render('back'); });
 });
 
-/*
-app.use(busboy());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.post('/file-upload', function(req, res) {
-    //console.log(req.body);
-    //console.log(req.files);
-    var fstream;
-        req.pipe(req.busboy);
-        req.busboy.on('file', function (fieldname, file, filename) {
-            console.log("Uploading: " + filename);
-
-            //Path where image will be uploaded
-            fstream = fs.createWriteStream(__dirname + '/public_files/' + filename);  
-            file.pipe(fstream); // POST data to the file
-            fstream.on('close', function () {    
-                console.log("Upload Finished of " + filename);              
-                
-                  
-                console.log(req.files.thumbnail.size);
-                //res.redirect('back');           //where to go next
-            });
-        });
-
-        req.busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated) {
-          console.log('Field [' + fieldname + ']: value: ' + inspect(val));
-        });
-}); */
 
 
 
