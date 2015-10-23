6acee44dcfa782fe6606db67c77d9292ab5419f0
Vincent Duong <vncntduong@gmail.com>
2015-10-22 21:20:36 -0700

    removed some unused stuff

--- index.js
+++ index.js
@@ -1,28 +1,14 @@
 var express = require('express');
-var request = require('request');
-var bodyParser = require('body-parser')
-var busboy = require('connect-busboy');
 var app = express();
-var path = require('path');
 var fs = require('fs');
 var multer = require('multer');
 
 
 app.set('port', (process.env.PORT || 4500));
 
-var userg = "init"
-
 app.set('views', 'views')
 app.set('view engine', 'jade');
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
-
-
 app.use(express.static('public'))
-
 
 //Get values from Form:'TestJSON' and pass a JsonObject back to jade -- Newman
 app.post('/',function(req,res)
@@ -36,33 +22,6 @@ app.get('/', function (req, res) {
   res.render('index', { title: "TITLE"});
 });
 
-
-
-
-app.get('/showList',function(req, res){
-  var myDB = require('./public/js/database.js');
-  var myQuery = "select * from planeinfo where max_speed IS NOT NULL AND msrp IS NOT NULL";
-  //var myRows = myDB.queryDB();
-  //var myRows ;
-  myDB.queryDB(myQuery, function(myRows){
-    if (myRows == null){
-     console.log("Couldnt access database");
-    }
-
-    else{
-      console.log("Rendering");
-      //console.log(myRows);
-      res.render('showList', {
-        "showList" : myRows
-      });
-    }
-
-  });
- 
-});
-
-
-
 app.get('/Uploaded_Files', function(req, res){
   var fileList = fs.readdirSync('public_files');
  fileList.splice(0,1);
@@ -72,13 +31,8 @@ app.get('/Uploaded_Files', function(req, res){
   });
 });
 
-
-
-
 app.get('/uploadPage', function(req, res){
-
   res.render('uploadPage.jade');
-  //res.end();
 });
 
 
@@ -99,34 +53,34 @@ app.post('/file-upload', file_uploaded.single('displayImage'), function(req, res
   var target_path = 'public_files/' + req.file.originalname;
   var src = fs.createReadStream(tmp_path);
   src.on('data', function(fileData){
-    // do the parsing and upload to DB here.. 
+    // do the parsing and upload to DB here..
 
     textBuff = fileData.toString();
   });
     // uploaded successfully
-  src.on('end', function() { 
+  src.on('end', function() {
     // remove src??
     console.log("File Loading Complete!");
-  
+
     // add textBuff into DB
     var myDB = require('./public/js/database.js');
     myDB.insertTable(req.file.originalname, textBuff, function(myRows){
-    
+
       if (myRows == true){
         console.log("insert success");
       }
 
       else{
-        console.log("insert fail");  
+        console.log("insert fail");
       }
 
     });
     res.render('uploadPage', {
         "fileData" : textBuff
     });
-    
+
   });
-  // failed to upload 
+  // failed to upload
   src.on('error', function(err) { res.render('back'); });
 });
 
@@ -134,10 +88,6 @@ app.post('/file-upload', file_uploaded.single('displayImage'), function(req, res
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
-server.listen((process.env.PORT || app.get('port')), function(){
-//server.listen(4501, function(){ 
-  console.log("Express server listening on port %d ", server.address().port);
-});
 
 app.get('/scatter',function(req,res){
   res.render('scatter', { title: "scatter"});
@@ -145,12 +95,9 @@ app.get('/scatter',function(req,res){
 
 app.get('/bars',function(req, res){
   var client = require('./public/js/database.js');
-  
   if (client == null)
-    console.log("Why!??!");
+    console.log("Where is Client?");
   else {
-
-    //client.query("SELECT * FROM planeinfo", function(err, rows){
     client.queryDB("select * from randnum" , function(myRows){
       if (myRows == null){
         console.log("Couldnt access database");
@@ -172,7 +119,7 @@ app.get('/bars',function(req, res){
 io.sockets.on('connection', function(socket){
   console.log("inside connection");
   socket.on('deleteFile', function(fileName){
-  
+
     var tableName = fileName.replace(/ /g, "_");
     tableName = tableName.substr(0, tableName.length-4);
     var myDB = require('./public/js/database.js');
@@ -185,12 +132,12 @@ io.sockets.on('connection', function(socket){
     // delete the physical file
     fs.unlinkSync('public_files/'.concat(fileName));
     socket.emit('doneDelete', dropSuccess);
-    
+
   });
 });
 
 
-/*
-app.listen(app.get('port'), function(){
-  console.log('app now running on port', app.get('port'))
-});*/
+
+server.listen((process.env.PORT || app.get('port')), function(){
+  console.log("Express server listening on port %d ", server.address().port);
+});
