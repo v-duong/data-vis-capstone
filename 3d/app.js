a9600fdf6a5c7b99178de7a6ec7934691989df1e
Jiapei Yao <jiapeiyao@umail.ucsb.edu>
2015-10-26 13:28:05 -0700

    working on canvas

--- 3d/app.js
+++ 3d/app.js
@@ -4,7 +4,7 @@ var app = express();
 app.engine('.html', require('ejs').__express);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
-
+app.use(express.static('public'));
 
 var server = app.listen(3000, function() {
 	console.log("listening on port %d", server.address().port);
@@ -14,9 +14,22 @@ app.get("/hello.html", function(req, res) {
 	res.send("<!DOCTYPE html><html>Hello World!</html>");
 });
 
-app.get("/", function(req, res) {
-	res.render('3d', {'pic1': input1, 'pic2': input2 })
+app.get("/3dviews", function(req, res) {
+	res.render('3dviews', {pic1: "pic2.jpg", pic2: "pic2.jpg" })
+});
 
+app.get("/sample", function(req, res) {
+	res.render('sample', {"Source" : "js/three.min.js"})
+});
+
+app.get("/", function(req, res) {
+	res.send("<!DOCTYPE html>\
+		<html>\
+		<body>\
+		<img src= '"+ 'pic.jpeg' +"' alt='a' style='width:228px;height:228px;' >\
+		<img src= '"+ 'https://cdn.chv.me/images/thumbnails/DIY_3D_Google_Cardboard_L2Jp8G8L.jpg.thumb_400x400.jpg' +"' alt='b' style='width:228px;height:228px;'>\
+		</body>\
+		</html>");
 });
 
 // app.get('/', function(req, res) {
