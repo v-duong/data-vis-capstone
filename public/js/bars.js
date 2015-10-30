9a3316d92b730e587174278bf04d6c79c5651cf8
Vincent Duong <vncntduong@gmail.com>
2015-10-29 17:10:57 -0700

    new templating; starting to modify all the visualization

--- public/js/bars.js
+++ public/js/bars.js
@@ -1,95 +1,82 @@
-var camera, scene, renderer,
-    geometry, material, mesh;
+var meshes = [];
 
-    init();
-    animate();
+function initbars() {
 
-    function init() {
+  document.addEventListener('mousedown', onDocumentMouseDown_Ortho, false);
 
-        document.addEventListener( 'mousedown', onDocumentMouseDown_Ortho, false );
-        scene = new THREE.Scene();
+  //camera = new THREE.PerspectiveCamera( 85, window.innerWidth / window.innerHeight, 1, 10000 );
+  camera = new THREE.OrthographicCamera(window.innerWidth / -2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / -2, 1, 100000);
 
-        //camera = new THREE.PerspectiveCamera( 85, window.innerWidth / window.innerHeight, 1, 10000 );
-        camera = new THREE.OrthographicCamera( window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, 1, 100000);
+  camera.position.z = 800;
+  camera.position.y = 600;
+  camera.position.x = 600;
+  camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-        camera.position.z = 800;
-        camera.position.y = 600;
-        camera.position.x = 600;
-        camera.lookAt(new THREE.Vector3(0,0,0));
-
-        controls = new THREE.OrbitControls( camera );
-//        controls.damping = 0.2;
-        controls.addEventListener( 'change', render );
+  controls = new THREE.OrbitControls(camera);
+  //				controls.damping = 0.2;
+  controls.addEventListener('change', render);
 
 
-        geometry3 = new THREE.BoxGeometry( 2400, 10, 2000 );
-        material3 = new THREE.MeshBasicMaterial( { color: 0xa0afaf} );
+  geometry3 = new THREE.BoxGeometry(2400, 10, 2000);
+  material3 = new THREE.MeshBasicMaterial({
+    color: 0xa0afaf
+  });
 
-        mesh3 = new THREE.Mesh( geometry3, material3 );
+  mesh3 = new THREE.Mesh(geometry3, material3);
 
+  scene.add(mesh3);
 
-        light = new THREE.DirectionalLight (0xffffff, 0.9 );
-        light.position.set (0,700,0);
-        light.castShadow = true;
-        light.shadowCameraNear = 1;
-        light.shadowCameraFar = 100000;
-        light.shadowMapWidth = 1024; // default is 512
-        light.shadowMapHeight = 1024; // default is 512
-        light.shadowCameraRight     =  window.innerWidth / 2;
-        light.shadowCameraLeft     = window.innerWidth / - 2;
-        light.shadowCameraTop      =  window.innerHeight / 2;
-        light.shadowCameraBottom   = window.innerHeight / - 2;
+  mesh3.position.y = -50;
 
-        scene.add( mesh3 );
-        scene.add( light );
+  renderer = new THREE.WebGLRenderer({
+    alpha: true
+  });
+  renderer.shadowMap.enabled = true;
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  renderer.setSize(window.innerWidth, window.innerHeight);
 
-        mesh3.position.y = -50;
+  $('.visual').append( renderer.domElement );
+}
 
-        renderer = new THREE.WebGLRenderer( { alpha: true } );
-        renderer.shadowMap.enabled = true;
-        renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-        renderer.setSize( window.innerWidth, window.innerHeight );
+function animate() {
 
-        document.body.appendChild( renderer.domElement );
-    }
+  requestAnimationFrame(animate);
+  render();
+}
 
-    function animate() {
-
-        requestAnimationFrame( animate );
-        render();
-    }
-
-    function render() {
-        renderer.render( scene, camera );
-    }
+function render() {
+  renderer.render(scene, camera);
+}
 
 
-    function addBar(x,y,z){
-      var frequency = 0.4;
-      geometry = new THREE.BoxGeometry( 49, y, 49 );
-      geometry.computeFaceNormals();
-      geometry.computeVertexNormals();
-      var gcolor = new THREE.Color(Math.sin(x*frequency), Math.sin(x*frequency + 2), Math.sin(x*frequency + 4));
-      material = new THREE.MeshBasicMaterial({color: gcolor});
-      geometry.colorsNeedUpdate = true;
-      mesh = new THREE.Mesh( geometry, material );
-      mesh.position.y = 10 + y/2;
-      mesh.position.x = x - 500;
-      mesh.position.z = z;
-      scene.add( mesh );
-      var edges = new THREE.EdgesHelper( mesh, 0x000000);
-      edges.material.linewidth = 2;
+function addBar(x, y, z) {
+  var frequency = 0.4;
+  geometry = new THREE.BoxGeometry(49, y, 49);
+  geometry.computeFaceNormals();
+  geometry.computeVertexNormals();
+  var gcolor = new THREE.Color(Math.sin(x * frequency), Math.sin(x * frequency + 2), Math.sin(x * frequency + 4));
+  material = new THREE.MeshBasicMaterial({
+    color: gcolor
+  });
+  geometry.colorsNeedUpdate = true;
+  mesh = new THREE.Mesh(geometry, material);
+  mesh.position.y = 10 + y / 2;
+  mesh.position.x = x - 500;
+  mesh.position.z = z;
+  scene.add(mesh);
+  var edges = new THREE.EdgesHelper(mesh, 0x000000);
+  edges.material.linewidth = 2;
 
-      scene.add(edges);
-    }
+  scene.add(edges);
+}
 
-    function createDictionary(_json){
-      if (typeof _json.x[0] == 'string')
-        var uniqx = _.uniq(_json.x);
-      if (typeof _json.y[0] == 'string')
-        var uniqy = _.uniq(_json.y);
-      if (typeof _json.z[0] == 'string')
-        var uniqz = _.uniq(_json.z);
+function createDictionary(_json) {
+  if (typeof _json.x[0] === 'string')
+    var uniqx = _.uniq(_json.x);
+  if (typeof _json.y[0] === 'string')
+    var uniqy = _.uniq(_json.y);
+  if (typeof _json.z[0] === 'string')
+    var uniqz = _.uniq(_json.z);
 
 
-    }
+}
