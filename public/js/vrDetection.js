2ce290b93bf0ce1bed54bec9a8a792d7cf193222
Jiapei Yao <jiapeiyao@umail.ucsb.edu>
2015-11-16 15:50:37 -0800

    trying safari fullscreen 5

--- public/js/vrDetection.js
+++ public/js/vrDetection.js
@@ -73,6 +73,10 @@ function enterVRMode(){
 	}
 	else if (element.requestFullscreen) {
 		element.requestFullscreen();
+	} else if (element.webkitRequestFullscreen) {
+		element.webkitRequestFullscreen();
+	} else if (element.mozRequestFullScreen) {
+		element.mozRequestFullScreen();
 	} else if (element.msRequestFullscreen) {
 		element.msRequestFullscreen();
 	}
