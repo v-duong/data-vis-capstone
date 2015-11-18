246d59ef89b9e7f3e5fdaa7249efac7b7ce79716
Vincent Duong <vncntduong@gmail.com>
2015-11-17 23:40:05 -0800

    * Deleted old pages * Moved libraries into new folder * Everything in public folder is now accessed through /static/ virtual directory * Now you don't need DB access to load index page.

--- index.js
+++ index.js
@@ -12,53 +12,30 @@ app.set('views', 'views');
 app.set('view engine', 'jade');
 
 
-app.use(express.static('public'))
+app.use('/static', express.static(__dirname + '/public'));
+
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
   extended: true
 }));
 
-
-
-
-// Database mapping
-// csv validation
-
-//Get values from Form:'TestJSON' and pass a JsonObject back to jade -- Newman
-var x = 0;
-var y = 0;
-var z = 0;
-
-app.post('/',function(req,res)
-  {
-    x = req.body.X; y = req.body.Y; z = req.body.Z;
-    res.redirect("/scatter");
-    res.end("yes");
-  });
-
-app.get('/', function (req, res) {
-
-  var client = require('./public/js/database.js');
-  var tlist;
-  client.queryDB("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';" , function(tlist){
-    res.render('index', { title: "TITLE", tables : tlist});
-  });
+app.get('/', function(req, res) {
+    res.render('index');
 });
 
 
-app.get('/Uploaded_Files', function(req, res){
+app.get('/Uploaded_Files', function(req, res) {
   //var fileList = fs.readdirSync('public_files');
   var getTableQuery = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'";
   var myDB = require('./public/js/database.js');
-  myDB.queryDB(getTableQuery, function(myTables){
-    if (myTables == null){
+  myDB.queryDB(getTableQuery, function(myTables) {
+    if (myTables == null) {
       console.log("uh oh");
-    }
-    else{
+    } else {
 
-       res.render('Uploaded_Files', {
-        "showFiles" : myTables
-        });
+      res.render('Uploaded_Files', {
+        "showFiles": myTables
+      });
     }
 
   });
@@ -66,73 +43,62 @@ app.get('/Uploaded_Files', function(req, res){
 
 });
 
-app.get('/uploadPage', function(req, res){
+app.get('/uploadPage', function(req, res) {
   res.render('uploadPage');
 });
 
 
 var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
+  destination: function(req, file, cb) {
     cb(null, 'public_files/');
   },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname );
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
   }
 });
 
 
-var file_uploaded = multer({ storage: storage });
+var file_uploaded = multer({
+  storage: storage
+});
 
-app.post('/file-upload', file_uploaded.single('datafile'), function(req, res){
-  if (req.file == null){
-     return;
+app.post('/file-upload', file_uploaded.single('datafile'), function(req, res) {
+  if (req.file == null) {
+    return;
   }
 
   var tmp_path = req.file.path;
   var target_path = 'public_files/' + req.file.originalname;
   var textBuff = "";
-  // reading Line by Line
-/*
-  var rl = require('readline').createInterface({
-    terminal: false,
-    input: fs.createReadStream(tmp_path)
-  });
-
-  rl.on('line', function(line){
-    console.log('1:', line);
-  });
-
-*/
   var src = fs.createReadStream(tmp_path);
 
-  src.on('data', function(fileData){
+  src.on('data', function(fileData) {
     textBuff = textBuff.concat(fileData.toString());
   });
 
 
 
-    // uploaded successfully
+  // uploaded successfully
   src.on('end', function() {
     // add textBuff into DB
 
     var myDB = require('./public/js/database.js');
 
-    myDB.insertTable(req.file.originalname, textBuff, function(myRows){
+    myDB.insertTable(req.file.originalname, textBuff, function(myRows) {
 
-      if (myRows == true){
+      if (myRows == true) {
         console.log("insert success");
         // delete the physical file
 
         res.render('uploadPage', {
-          "fileData" : textBuff
+          "fileData": textBuff
         });
 
-      }
-      else{
+      } else {
         console.log("insert fail");
         textBuff = "Upload Failed";
         res.render('uploadPage', {
-          "fileData" : textBuff
+          "fileData": textBuff
         });
 
       }
@@ -143,57 +109,51 @@ app.post('/file-upload', file_uploaded.single('datafile'), function(req, res){
 
 
   // failed to upload
-  src.on('error', function(err) { res.render('back'); });
+  src.on('error', function(err) {
+    res.render('back');
+  });
 
 });
 
 
 var server = require('http').createServer(app);
-server.listen((process.env.PORT || app.get('port')), function(){
+server.listen((process.env.PORT || app.get('port')), function() {
   console.log("Express server listening on port %d ", server.address().port);
 });
 
-
-
-// app.get('/scatter',function(req,res){
-//   res.render('scatter', {title: 'scatter', data1:x, data2:y, data3:z});
-// })
-
-app.get('/scatter',function(req, res){
+app.get('/scatter', function(req, res) {
   var client = require('./public/js/database.js');
   if (client == null)
     console.log("cannot get database");
   else {
-    client.queryDB("select * from smartphonestestexcel" , function(myRows){
-      if (myRows == null){
+    client.queryDB("select * from smartphonestestexcel", function(myRows) {
+      if (myRows == null) {
         console.log("query fail");
-      }
-      else{
-          res.render('scatter', {
-            _data : myRows
-          });
+      } else {
+        res.render('scatter', {
+          _data: myRows
+        });
       }
     });
   }
 });
 
-app.get('/bars',function(req, res){
+app.get('/bars', function(req, res) {
   var client = require('./public/js/database.js');
   if (client == null)
     console.log("Where is Client?");
   else {
-    client.queryDB("select * from randnum" , function(myRows){
-      if (myRows == null){
+    client.queryDB("select * from randnum", function(myRows) {
+      if (myRows == null) {
         console.log("Couldnt access database");
-      }
-      else{
+      } else {
         var currentRow;
-        for (var i in myRows){
+        for (var i in myRows) {
           currentRow = myRows[i];
         }
-          res.render('bars', {
-            _data : myRows
-          });
+        res.render('bars', {
+          _data: myRows
+        });
       }
     });
   }
@@ -201,43 +161,39 @@ app.get('/bars',function(req, res){
 
 
 
-app.get('/displayData',function(req, res){
+app.get('/displayData', function(req, res) {
   res.render('displayData');
 });
 
-app.get('/visualize',function(req, res){
+app.get('/visualize', function(req, res) {
   var client = require('./public/js/database.js');
   var tlist;
-  client.queryDB("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';" , function(tlist){
-    res.render('visualize', { tables : tlist});
+  client.queryDB("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public';", function(tlist) {
+    res.render('visualize', {
+      tables: tlist
+    });
   });
-
 });
 
-app.get('/retrieveData', function(req, res){
-    var myQuery = req.query.myQuery;
-    var myDB = require('./public/js/database.js');
-    myDB.queryDB(myQuery, function(myRows){
-      if (myRows == null){
-        console.log("Couldnt access database");
-      }
-      else{
-        //console.log(JSON.stringify(myRows))
-       res.send(JSON.stringify(myRows));
-      }
-    });
-
-
+app.get('/retrieveData', function(req, res) {
+  var myQuery = req.query.myQuery;
+  var myDB = require('./public/js/database.js');
+  myDB.queryDB(myQuery, function(myRows) {
+    if (myRows == null) {
+      console.log("Couldnt access database");
+    } else {
+      //console.log(JSON.stringify(myRows))
+      res.send(JSON.stringify(myRows));
+    }
+  });
 });
 
-
-
-app.post('/delData', function(req, res){
-    var tableName = req.body.tName;
-    var myDB = require('./public/js/database.js');
-    var dropSuccess = false;
-    myDB.deleteTable(tableName, function(dropErr){
-      res.send(JSON.stringify(dropErr));
-    });
+app.post('/delData', function(req, res) {
+  var tableName = req.body.tName;
+  var myDB = require('./public/js/database.js');
+  var dropSuccess = false;
+  myDB.deleteTable(tableName, function(dropErr) {
+    res.send(JSON.stringify(dropErr));
+  });
 
 });
