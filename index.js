8971307d81f2e35c5f7b5a2a1c5840607600b3a4
Vincent Duong <vncntduong@gmail.com>
2016-01-26 12:49:35 -0800

    login stuff fix, ui changes

--- index.js
+++ index.js
@@ -35,6 +35,8 @@ app.use(session({secret: 'secret as shit', resave: false, saveUninitialized: fal
 app.use(passport.initialize());
 app.use(passport.session());
 
+require('./passport.js')(passport, Strategy, bcrypt, sequelize);
+
 app.get('*', function(req, res, next) {
   res.locals.logged = (req.user) ? true : false
   next();
@@ -54,13 +56,13 @@ app.get('/logout', function(req, res) {
 })
 
 app.post('/auth/login', passport.authenticate('login', {
-  successRedirect: '/files',
+  successRedirect: '/',
   failureRedirect: '/login',
   failureFlash : true
 }));
 
 app.post('/auth/register', passport.authenticate('register', {
-  successRedirect: '/files',
+  successRedirect: '/',
   failureRedirect: '/login',
   failureFlash : true
 }));
@@ -338,60 +340,3 @@ var server = require('http').createServer(app);
 server.listen((process.env.PORT || app.get('port')), function() {
   console.log("Express server listening on port %d ", server.address().port);
 });
-
-
-
-
-
-
-
-
-
-
-var User = sequelize.define('User', {
-  username: {type: Sequelize.STRING, unique: true},
-  password: Sequelize.STRING,
-  usertables: Sequelize.ARRAY(Sequelize.TEXT)
-});
-
-User.sync();
-
-passport.serializeUser(function(user, done) {
-  console.log(user.id)
-  done(null, user.id);
-});
-
-passport.deserializeUser(function(id, done) {
-  User
-    .findById(id).then( function(user) {
-      console.log(id)
-      if (!user) {
-        return done(null, false);
-      }
-      //console.log(user)
-      done(null, user);
-    });
-});
-
-passport.use('register', new Strategy({usernameField: 'user', passwordField: 'pass', passReqToCallback: true},function(req, user, pass, done) {
-  User
-    .findOrCreate({where: {username: user}, defaults: {password: bcrypt.hashSync(pass, 5), usertables: []} }).spread(function(u,c) {
-      if (!c) {
-        return done(null, false, {message: "username taken"})
-      } else {
-          return done(null, u);
-        }
-      })
-    })
-)
-
-passport.use('login', new Strategy({usernameField: 'user', passwordField: 'pass', passReqToCallback: true}, function(req, user, pass, done) {
-  User
-    .findOne( { where: {username: user} } ).then( function(u) {
-      if (!u)
-        return done(null, false, {message: "user not found"})
-      if (!bcrypt.compareSync(pass, u.password))
-        return done(null, false, {message: "wrong password"})
-      return done(null, u)
-    })
-}))
--- passport.js
+++ passport.js
@@ -1,56 +1,53 @@
 var Sequelize = require('sequelize');
 
 
-module.exports = function(passport, Strategy, bcrypt, db){
+module.exports = function(passport, Strategy, bcrypt, sequelize){
 
-var User = db.define('User', {
-  username: {type: Sequelize.STRING, unique: true},
-  password: Sequelize.STRING,
-  usertables: Sequelize.ARRAY(Sequelize.TEXT)
-});
+  var User = sequelize.define('User', {
+    username: {type: Sequelize.STRING, unique: true},
+    password: Sequelize.STRING,
+    usertables: Sequelize.ARRAY(Sequelize.TEXT)
+  });
 
-User.sync();
+  User.sync();
 
-passport.serializeUser(function(user, done) {
-  console.log(user.id)
-  done(null, user.id);
-});
+  passport.serializeUser(function(user, done) {
+    console.log(user.id)
+    done(null, user.id);
+  });
 
-passport.deserializeUser(function(id, done) {
-  User
-    .findById(id).then( function(user) {
-      console.log(id)
-      if (!user) {
-        return done(null, false);
-      }
-      done(null, user);
-    });
-});
-
-passport.use('register', new Strategy({usernameField: 'user', passwordField: 'pass', passReqToCallback: true}, function(req, user, pass, done) {
-  console.log("REgister")
-  User
-    .findOrCreate({where: {username: user}, defaults: {password: pass, usertables: []} }).spread(function(u,c) {
-      if (!c) {
-        console.log("fauikl")
-        return done(null, false, {message: "username taken"})
-      } else {
-        console.log("WHAT")
-          return done(null, u);
+  passport.deserializeUser(function(id, done) {
+    User
+      .findById(id).then( function(user) {
+        console.log(id)
+        if (!user) {
+          return done(null, false);
         }
+        //console.log(user)
+        done(null, user);
+      });
+  });
+
+  passport.use('register', new Strategy({usernameField: 'user', passwordField: 'pass', passReqToCallback: true},function(req, user, pass, done) {
+    User
+      .findOrCreate({where: {username: user}, defaults: {password: bcrypt.hashSync(pass, 5), usertables: []} }).spread(function(u,c) {
+        if (!c) {
+          return done(null, false, {message: "Username is already in use."})
+        } else {
+            return done(null, u);
+          }
+        })
       })
-    })
-)
+  )
 
-passport.use('login', new Strategy({usernameField: 'user', passwordField: 'pass', passReqToCallback: true}, function(req, user, pass, done) {
-  User
-    .findOne( { where: {username: user} } ).then( function(u) {
-      if (!u)
-        return done(null, false, {message: "user not found"})
-      if (!bcrypt.compareSync(pass, u.password))
-        return done(null, false, {message: "wrong password"})
-      return done(null, u)
-    })
-}))
-
+  passport.use('login', new Strategy({usernameField: 'user', passwordField: 'pass', passReqToCallback: true}, function(req, user, pass, done) {
+    User
+      .findOne( { where: {username: user} } ).then( function(u) {
+        if (!u)
+          return done(null, false, {message: "Login failed: Incorrect credentials."})
+        if (!bcrypt.compareSync(pass, u.password))
+          return done(null, false, {message: "Login failed: Incorrect credentials."})
+        return done(null, u)
+      })
+  }))
 }
