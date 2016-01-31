993cee2a571e9e04fbe89746f76d6a43c5d6632f
kmai12 <kevinmai993@yahoo.com>
2016-01-30 22:16:36 -0800

    column/filter hiding for globe added

--- public/js/filters.js
+++ public/js/filters.js
@@ -120,6 +120,11 @@ function setDefaultDropDownValue(visSelected, col1, col2, col3, colList){
 $("#VisualList").change(function(){
   var visualSelected =  $("#VisualList option:selected").val();
   var tableSelected = $("#TableList option:selected").text();
+
+  $('#filters').hide();
+  $('#columnOption').show();
+  $('#columnSelection').show();
+  $('#filtersOption').show();
   console.log(tableSelected);
   switch(visualSelected){
   // if we're switching to basketball, theres no filters, so make sure to remove all
@@ -139,12 +144,22 @@ $("#VisualList").change(function(){
       if (tableSelected != 'Choose Table')
         createColsScatter(visualSelected, tableSelected);
       break;
-    default:
+    case 'globe':
+      hideColumnOptions();
+
       break;
 
   }
 
 });
+
+function hideColumnOptions(){
+  $('#filters').hide();
+  $('#columnOption').hide();
+  $('#columnSelection').hide();
+  $('#filtersOption').hide();
+}
+
 
 function createColsBar(visualSelected, tableSelected){
   //var getColumnTypeQuery = "SELECT column_name ,data_type FROM information_schema.columns where table_name = '";
