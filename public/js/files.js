9d18b297f8d030e8980194738651b11b748d4242
Vincent Tan <vwtan@umail.ucsb.edu>
2016-01-31 16:39:17 -0800

    seperate javascript code from jade

---
+++ public/js/files.js
@@ -0,0 +1,67 @@
+window.onload = function(){
+  $.get('/tables', function(data){
+    var d = JSON.parse(data);
+    for (i = 0; i < d.length; i++)
+      $('.filetable').append('<tr><td>'+ d[i].table_name +'</td><td>\
+      <input class="button" type="button" class="delbutton" id='+ d[i].table_name +' value="Delete" onclick="deleteFunc(this.id)">\
+      </td></tr>')
+  });
+
+  $( '.file-input' ).each( function(){
+    var $input   = $( this ),
+      $label   = $input.next( 'label' ),
+      labelVal = $label.html();
+
+    $input.on( 'change', function( e )
+    {
+      var fileName = '';
+
+      if( this.files && this.files.length > 1 )
+        fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+      else if( e.target.value )
+        fileName = e.target.value.split( '\\' ).pop();
+
+      if( fileName ){
+        $label.find( 'span' ).html( fileName );
+        $('#upload').removeAttr('disabled');
+      }
+      else {
+        $label.html( labelVal );
+        $('#upload').attr('disabled', 'disabled');
+      }
+    });
+
+    // Firefox bug fix
+    $input
+    .on( 'focus', function(){ $input.addClass( 'has-focus' ); })
+    .on( 'blur', function(){ $input.removeClass( 'has-focus' ); });
+  });
+}
+
+var CustDropzone = new Dropzone(document.body, { // Make the whole body a dropzone
+  url: "/files", // Set the url
+  paramName: "datafile", // The name that will be used to transfer the file
+  maxFilesize: 2, // MB
+  init: function () {
+    // Set up any event handlers
+    this.on('complete', function () {
+        console.log("ON COMPLETE");
+        location.reload();
+    });
+  }
+});
+
+
+
+function deleteFunc(btnID){
+  var fileName = btnID;
+  $.post('/delData', { tName: fileName }, function(data){
+    console.log(data);
+    if (data == true){
+      location.reload();
+    }
+    else {
+      console.log("It died");
+    }
+  }, 'json');
+}
